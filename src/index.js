a8046b6a8908e1ba8259ea9e45f8bac550b8e7cc
Chhavi Arora <chhaviarora1401@gmail.com>
2021-12-04 01:27:02 +0530

    Mini Book Project

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Booklist() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img src="https://m.media-amazon.com/images/I/81UoflSDrCL._AC_UY327_FMwebp_QL65_.jpg" alt="" />
+);
+const Title = () => <h1>The Mother of All Booklists: The 500 Most Recommended Nonfiction Reads for Ages 3 to 103 </h1>;
+const Author = () => <h4> by William Patrick Martin</h4>;
+
+ReactDOM.render(<Booklist />, document.getElementById('root'));
