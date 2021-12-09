6c093093b203956cd253d50992c5f7b98974d9c8
Chhavi Arora <chhaviarora1401@gmail.com>
2021-12-09 23:34:29 +0530

    Props

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//CSS
+// CSS
 import './index.css';
+// setup vars
+const firstBook = {
+  author: 'by William Patrick Martin',
+  title: 'The Mother of All Booklists: The 500 Most Recommended Nonfiction Reads for Ages 3 to 103',
+  img: 'https://m.media-amazon.com/images/I/81UoflSDrCL._AC_UY327_FMwebp_QL65_.jpg'
+}
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/81kRsgayVDL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'The Richard Jackson Saga Booklist',
+  author: 'by Ed Nelson'
+}
 
 function Booklist() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
     </section>
   );
 }
 
-const author = 'by William Patrick Martin';
-const Book = () => {
-  const title = 'The Mother of All Booklists: The 500 Most Recommended Nonfiction Reads for Ages 3 to 103';
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
-      <img src="https://m.media-amazon.com/images/I/81UoflSDrCL._AC_UY327_FMwebp_QL65_.jpg" alt="" />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p> {let x = 6}</p> error */}
-      <p>{6 + 6}</p>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
