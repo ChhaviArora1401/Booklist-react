99548a6d11dac5fc6850ff70debc4da58fbe9a9a
Chhavi Arora <chhaviarora1401@gmail.com>
2021-12-10 23:19:41 +0530

    Simple List

--- src/index.js
+++ src/index.js
@@ -4,46 +4,37 @@ import ReactDOM from 'react-dom';
 // CSS
 import './index.css';
 // setup vars
-const firstBook = {
-  author: 'by William Patrick Martin',
-  title: 'The Mother of All Booklists: The 500 Most Recommended Nonfiction Reads for Ages 3 to 103',
-  img: 'https://m.media-amazon.com/images/I/81UoflSDrCL._AC_UY327_FMwebp_QL65_.jpg'
-}
+const books = [
+  {
+    author: 'by William Patrick Martin',
+    title: 'The Mother of All Booklists: The 500 Most Recommended Nonfiction Reads for Ages 3 to 103',
+    img: 'https://m.media-amazon.com/images/I/81UoflSDrCL._AC_UY327_FMwebp_QL65_.jpg'
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/81kRsgayVDL._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Richard Jackson Saga Booklist',
+    author: 'by Ed Nelson'
+  }
+];
 
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/81kRsgayVDL._AC_UY327_FMwebp_QL65_.jpg',
-  title: 'The Richard Jackson Saga Booklist',
-  author: 'by Ed Nelson'
-}
-
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+});
+console.log(newNames);
 function Booklist() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author} />
+      {newNames}
     </section>
   );
 }
 
 const Book = (props) => {
-  // const { img, title, author } = props OR
-  const { img, title, author, children } = props
+  const { img, title, author } = props
   return (
     <article className='book'>
       <img src={img} alt="" />
-      {/* {props.children} OR */}
-      {children}
       <h1>{title}</h1>
       <h4>{author}</h4>
     </article>
