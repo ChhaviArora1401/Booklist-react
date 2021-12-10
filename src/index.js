1948d6ba42ba9dcf5a223f526389fc58c578f656
Chhavi Arora <chhaviarora1401@gmail.com>
2021-12-11 00:10:58 +0530

    Proper list

--- src/index.js
+++ src/index.js
@@ -6,32 +6,49 @@ import './index.css';
 // setup vars
 const books = [
   {
-    author: 'by William Patrick Martin',
+    img: 'https://m.media-amazon.com/images/I/81UoflSDrCL._AC_UY327_FMwebp_QL65_.jpg',
     title: 'The Mother of All Booklists: The 500 Most Recommended Nonfiction Reads for Ages 3 to 103',
-    img: 'https://m.media-amazon.com/images/I/81UoflSDrCL._AC_UY327_FMwebp_QL65_.jpg'
+    author: 'by William Patrick Martin'
   },
   {
     img: 'https://m.media-amazon.com/images/I/81kRsgayVDL._AC_UY327_FMwebp_QL65_.jpg',
     title: 'The Richard Jackson Saga Booklist',
     author: 'by Ed Nelson'
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/814zUtStvQL._AC_UY218_.jpg',
+    title: 'They Fight Like Devils: Stories from Lucknow During the Great Mutiny, 1857-1858',
+    author: 'by D. A. Kinsley'
+  },
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/41loJ609dTL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'She Erased Her',
+    author: 'by Himanshu Rai'
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/61kGFiddWHL._AC_UY218_.jpg',
+    title: 'When Life tricked me',
+    author: 'by Vivek Khanna'
   }
 ];
 
-const names = ['john', 'peter', 'susan'];
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>
-});
-console.log(newNames);
+
 function Booklist() {
   return (
     <section className='booklist'>
-      {newNames}
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return (
+          <Book book={book}></Book>
+        );
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props
+  console.log(props);
+  const { img, title, author } = props.book;
   return (
     <article className='book'>
       <img src={img} alt="" />
